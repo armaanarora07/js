9cbb7eec1215e3f595cacb7cd09d6c5c030fc5b2
armaanarora07 <149007239+armaanarora07@users.noreply.github.com>
2024-10-28 12:23:03 +0000

    L3 conversions

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,36 @@
+//converting into number
+let score = 33
+
+let value_in_number = Number(score)
+// console.log(value_in_number);
+// console.log(typeof value_in_number)
+
+/*
+"33"--> 33
+"33abc" --> Nan
+True -->1 and false -->0
+undefined --> Nan
+Null --> 0
+*/
+
+
+//converting into boolean
+let is_looged_in= ""
+let boolean_is_looged_in = Boolean(is_looged_in)
+
+// console.log(boolean_is_looged_in);
+// console.log(typeof boolean_is_looged_in);
+
+// 0=> false, 1=>true
+// ""=> false, "abc123"=> true
+
+
+// conversion into string
+
+let some_number = 34
+
+string_some_number = String(some_number)
+
+console.log(string_some_number)
+console.log(typeof string_some_number)
+
