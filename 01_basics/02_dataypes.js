abe869eeb308208f741c50ca5e2210e17ed463ee
armaanarora07 <149007239+armaanarora07@users.noreply.github.com>
2024-10-28 09:22:28 +0000

    L2 datatypes

---
+++ 01_basics/02_dataypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert(1+1) different working in nodejs, it will work in browser
+
+// console.log(3+3); console.log("armaan")
+
+//primitve datatype
+
+//number= 2 to the power 53
+//bigInt
+//string -->""
+//boolean --> true or false
+//null --> standalone value
+// You can assign null to variables, pass it as an argument to functions, and return it from functions. It is commonly used in scenarios where you want to indicate that a variable should not hold any value.
+//undefined --> no value is assigned
+//symbol --> for finding uniquessness
+
+
+console.log(typeof null); //obeject 
+console.log(typeof undefined); //undefined
