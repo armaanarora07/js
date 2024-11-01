83bf030be12cfc2309f930a3602ef7d8813b7b12
armaanarora07 <149007239+armaanarora07@users.noreply.github.com>
2024-11-01 08:16:00 +0000

    L8 Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//comparison <,>,<= and eqaulity check == works differnelty where comparison coverts null into 0 and equality check doesnt
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//avoid above comparisons they the the inconsistency of the langauge iteself
+
+
+// ===  (strict check)
+
+// console.log("0"==0);
+// console.log("0"===0);
+
+
+
+
